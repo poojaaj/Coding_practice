73ebc21a565baacbc1396d2af5405fe898e43303
Pooja Jeergyal <poojaashokjeergyal@gmail.com>
2020-05-27 22:18:40 -0500

    Find if the array contains any duplicates

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,11 +1,23 @@
-def containsDuplicate(num):
-    j = 0
-    val = False
-    for i in range(1,len(nums)):
-        while j != len(nums)-1 :
-            if nums[i] == nums[j]:
-            val = True
-    return val
+def containsDuplicate(nums):
+    if len(set(nums)) != len(nums):
+        return True
+    else:
+        return False
 
-nums = [1,2,3,1]
+nums = [2,14,18,22,22]
 print(containsDuplicate(nums))
+
+
+#altrnative method
+# def containsDuplicate(nums):
+#     l = []
+#     for i in nums:
+#         if i not in l:
+#             l.append(i)
+#     if len(l) != len(nums):
+#         return True
+#     else:
+#         return False
+
+# nums = [1,2,3,4]
+# print(containsDuplicate(nums))
