f15ebf278f12886916f056ee0529e28b6cc9100a
Pooja Jeergyal <poojaashokjeergyal@gmail.com>
2020-06-24 17:17:23 -0500

    bitwise XOR of all elements of nums.

--- xorOperation.py
+++ xorOperation.py
@@ -1,3 +1,11 @@
+#Given an integer n and an integer start.
+
+#Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+#Return the bitwise XOR of all elements of nums.
+
+
+
 def xorOperation(n, start):
     nums = []
     xor_arr = 0
